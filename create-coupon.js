8e5798a7e84358ad6b7aa729261a7b1df801ea9d
Isabela Osaki <53798461+iosaki@users.noreply.github.com>
2024-04-18 18:42:41 -0300

    Update create-coupon.js

--- create-coupon.js
+++ create-coupon.js
@@ -1,4 +1,1 @@
- // Função para exibir o alerta quando o usuário clicar em qualquer lugar da página
-        document.addEventListener("click", function() {
-            alert("Você clicou em algum lugar da página!");
-        });
+
