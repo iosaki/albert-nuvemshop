f9ea3577252d322c8a28f379aeafbf07970a476b
Isabela Osaki <53798461+iosaki@users.noreply.github.com>
2024-04-18 18:50:55 -0300

    Update create-coupon.js

--- create-coupon.js
+++ create-coupon.js
@@ -1,1 +1,14 @@
+  function addParagraph() {
+    var couponInput = document.getElementById('coupon-input');
+    if (couponInput) {
+      var newParagraph = document.createElement('p');
+      newParagraph.classList.add('new-paragraph');
+      newParagraph.innerText = 'Teste de script';
+      couponInput.parentNode.appendChild(newParagraph);
+    }
+  }
 
+  // Adiciona o parágrafo quando qualquer lugar da página é clicado
+  document.addEventListener('click', function() {
+    addParagraph();
+  });
