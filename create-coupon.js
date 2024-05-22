76292cad2e03db0af1472d999133b60a69c70d7d
Isabela Osaki <53798461+iosaki@users.noreply.github.com>
2024-05-22 11:43:57 -0300

    Update create-coupon.js

--- create-coupon.js
+++ create-coupon.js
@@ -1,87 +1,114 @@
+function sendPayload(email, cartId) {
+    const userEmail = document.getElementById('reviewBlockContentEmail').textContent;
+    const payload = {
+        cart_id: cartId,
+        email: email,
+    };
+
+    // Use Fetch API or any preferred library to send POST request
+    fetch("https://hook.us1.make.com/wzmj4fu7dw7brhwpkclvi33kfuok92yk", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Payload sent:", data);
+        })
+        .catch((error) => {
+            console.error("Error sending payload:", error);
+        });
+}
+
+
 function urlContainsCheckout() {
-  return window.location.href.includes("/checkout/v3/next/");
+    return window.location.href.includes("/checkout/v3/next/");
 }
 
 if (urlContainsCheckout()) {
-  // Function to create the modal
-  function createModal() {
-    var modal = document.createElement("div");
-    modal.className = "modal";
+    // Function to create the modal
+    function createModal() {
+        var modal = document.createElement("div");
+        modal.className = "modal";
 
-    var modalContent = document.createElement("div");
-    modalContent.className = "modal-content";
+        var modalContent = document.createElement("div");
+        modalContent.className = "modal-content";
 
-    var closeButton = document.createElement("span");
-    closeButton.textContent = "×";
-    closeButton.className = "close-icon";
+        var closeButton = document.createElement("span");
+        closeButton.textContent = "×";
+        closeButton.className = "close-icon";
 
-    var logoImg = document.createElement("img");
-    logoImg.src = "https://uploads-ssl.webflow.com/611e7d70d4e879564857dd9d/66154344f41761f4afc3bf7e_albert_logo_com_tagline_1.png";
-    logoImg.alt = "Logo";
-    logoImg.width = 148.66;
-    logoImg.height = 83.5;
+        var logoImg = document.createElement("img");
+        logoImg.src = "https://uploads-ssl.webflow.com/611e7d70d4e879564857dd9d/66154344f41761f4afc3bf7e_albert_logo_com_tagline_1.png";
+        logoImg.alt = "Logo";
+        logoImg.width = 148.66;
+        logoImg.height = 83.5;
 
-    var cashbackInfo = document.createElement("p");
-    // Get the cashback value and replace "XX" in the text
-    cashbackInfo.textContent = "Hey! Tem cashback na sua carteira e você pode usá-lo como parte do pagamento.\nSeu saldo disponível para uso é de R$XX.";
-    cashbackInfo.style.marginBottom = "20px";
+        var cashbackInfo = document.createElement("p");
+        // Get the cashback value and replace "XX" in the text
+        cashbackInfo.textContent = "Hey! Tem cashback na sua carteira e você pode usá-lo como parte do pagamento.\nSeu saldo disponível para uso é de R$XX.";
+        cashbackInfo.style.marginBottom = "20px";
 
-    var yesButton = document.createElement("button");
-    yesButton.textContent = "Quero usar nessa compra";
-    yesButton.classList.add("button", "yes-button");
+        var yesButton = document.createElement("button");
+        yesButton.textContent = "Quero usar nessa compra";
+        yesButton.classList.add("button", "yes-button");
 
-    var noButton = document.createElement("button");
-    noButton.textContent = "Não quero usar agora";
-    noButton.classList.add("button", "no-button");
+        var noButton = document.createElement("button");
+        noButton.textContent = "Não quero usar agora";
+        noButton.classList.add("button", "no-button");
 
-    modalContent.appendChild(closeButton);
-    modalContent.appendChild(logoImg);
-    modalContent.appendChild(cashbackInfo);
-    modalContent.appendChild(yesButton);
-    modalContent.appendChild(noButton);
-    modal.appendChild(modalContent);
 
-    document.body.appendChild(modal);
+        modalContent.appendChild(closeButton);
+        modalContent.appendChild(logoImg);
+        modalContent.appendChild(cashbackInfo);
+        modalContent.appendChild(yesButton);
+        modalContent.appendChild(noButton);
+        modal.appendChild(modalContent);
 
-    closeButton.addEventListener("click", function () {
-      document.body.removeChild(modal);
+        document.body.appendChild(modal);
+
+        closeButton.addEventListener("click", function() {
+            document.body.removeChild(modal);
+        });
+
+        // Event listener for "Não quero usar agora" button
+        noButton.addEventListener("click", function() {
+            sendPayload(userEmail, getCartIdFromUrl()); // Replace with function to get cartId
+
+        });
+
+
+        // Add overlay layer
+        var overlay = document.createElement("div");
+        overlay.className = "overlay";
+        document.body.appendChild(overlay);
+
+        // Show loading animation
+        var loadingAnimation = document.createElement("div");
+        loadingAnimation.className = "loading-animation";
+        loadingAnimation.innerHTML = "<div class='loader'></div>";
+        document.body.appendChild(loadingAnimation);
+
+        // Wait 3 seconds and reload page without cache
+        setTimeout(function() {
+            document.body.removeChild(overlay);
+            document.body.removeChild(loadingAnimation);
+            location.reload(true);
+        }, 3000);
     });
+}
 
-    // Event listener for "Não quero usar agora" button
-    noButton.addEventListener("click", function () {
-      // Disable buttons
-      yesButton.disabled = true;
-      noButton.disabled = true;
-
-      // Add overlay layer
-      var overlay = document.createElement("div");
-      overlay.className = "overlay";
-      document.body.appendChild(overlay);
-
-      // Show loading animation
-      var loadingAnimation = document.createElement("div");
-      loadingAnimation.className = "loading-animation";
-      loadingAnimation.innerHTML = "<div class='loader'></div>";
-      document.body.appendChild(loadingAnimation);
-
-      // Wait 3 seconds and reload page without cache
-      setTimeout(function () {
-        document.body.removeChild(overlay);
-        document.body.removeChild(loadingAnimation);
-        location.reload(true);
-      }, 3000);
-    });
-  }
-
-  function firstClickHandler() {
+function firstClickHandler() {
     createModal();
     document.removeEventListener("click", firstClickHandler);
-  }
+}
 
-  document.addEventListener("click", firstClickHandler);
+document.addEventListener("click", firstClickHandler);
 
-  var style = document.createElement("style");
-  style.textContent = `
+var style = document.createElement("style");
+style.textContent = `
     .modal {
       position: fixed;
       top: 50%;
@@ -180,5 +207,5 @@ if (urlContainsCheckout()) {
       100% { transform: rotate(360deg); }
     }
   `;
-  document.head.appendChild(style);
+document.head.appendChild(style);
 }
