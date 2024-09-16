70afffd7b7ca19449d21f91e01c5bfc5947467eb
Isabela Osaki <53798461+iosaki@users.noreply.github.com>
2024-09-16 15:54:15 -0300

    Update store.js

--- store.js
+++ store.js
@@ -1,147 +1,140 @@
-function urlContainsCheckout() {
-    return window.location.href.includes("/checkout/v3/next/");
+function criarBalao() {
+  // Criar elemento div para o balão de texto
+  var balao = document.createElement("div");
+  balao.className = "balao";
+
+  // Adicionar ícone de fechar com SVG
+  var closeButton = document.createElement("button");
+  closeButton.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
+        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 7l10 10m-10 0l10-10"/>
+      </svg>`;
+  closeButton.className = "close-button";
+  closeButton.style.position = "absolute";
+  closeButton.style.top = "10px";
+  closeButton.style.right = "10px";
+  closeButton.style.cursor = "pointer";
+  closeButton.style.border = "none";
+  closeButton.style.backgroundColor = "transparent";
+  closeButton.addEventListener("click", esconderBalao);
+  balao.appendChild(closeButton);
+
+  // Adicionar logo dentro do balão
+  var logo = document.createElement("img");
+  logo.src =
+    "https://albert-mkt.s3.amazonaws.com/albert+muito+ale%CC%81m+de+cashback.png";
+  logo.alt = "Logo";
+  logo.style.height = "70px";
+  logo.style.display = "block"; // Para centralizar horizontalmente
+  logo.style.margin = "0 auto"; // Para centralizar horizontalmente
+  logo.style.width = "auto";
+  balao.appendChild(logo);
+
+  // Adicionar texto dentro do balão
+  var texto = document.createElement("p");
+  texto.innerHTML = `
+  <strong>Tem cashback te esperando no final da compra!</strong><br><br>Faça o login por aqui com o mesmo e-mail que cadastrou no Albert ou clique abaixo para se cadastrar
+`;
+  texto.style.margin = "10px 10px 30px 10px";
+  texto.style.textAlign = "justify"; // Adicionando o estilo para justificar o texto
+
+  balao.appendChild(texto);
+
+  // Criar botão dentro do balão
+  var botao = document.createElement("a");
+  botao.style.textDecoration = "none";
+  botao.textContent = "Quero cashback";
+  botao.href =
+    "https://associados.oialbert.com.br/criar-conta?code=SEJAASSOCIADO";
+  botao.target = "_blank"; // Abrir em uma nova aba
+  botao.style.background = "linear-gradient(90deg, #ff281e, #ff005a)";
+  botao.style.color = "#fff";
+  botao.style.borderRadius = "10px"; // Aumentando o raio para deixar as bordas mais arredondadas
+  botao.style.padding = "10px";
+  botao.style.cursor = "pointer";
+  botao.style.fontWeight = "bold";
+  balao.style.padding = "10px 20px";
+  balao.style.textAlign = "center"; // Para centralizar o botão horizontalmente
+
+  balao.appendChild(botao);
+
+  // Estilos CSS para o balão
+  balao.style.position = "fixed";
+  balao.style.height = "250px"; // Ajustado para acomodar o botão
+  balao.style.width = "500px";
+  balao.style.top = "75%";
+  balao.style.right = "30px"; // Ajuste para a margem direita
+  balao.style.transform = "translateY(-50%)";
+  balao.style.padding = "10px 20px";
+  balao.style.backgroundColor = "#ffff";
+  balao.style.border = "1px solid #ccc";
+  balao.style.borderRadius = "5px";
+  balao.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.2)";
+  balao.style.zIndex = "99";
+  balao.style.opacity = "0";
+
+  // Adicionar o balão à página
+  document.body.appendChild(balao);
+
+  return balao;
 }
 
-if (urlContainsCheckout()) {
-    // Função para verificar se o usuário está logado
-    function isUserLoggedIn() {
-        var loggedInElement = document.querySelector('a[href="/account"]');
-        return loggedInElement !== null;
+function mostrarBalao() {
+  var balao = document.querySelector(".balao");
+  // Transição de opacidade para mostrar o balão
+  balao.style.transition = "opacity 0.5s ease"; // Tempo e tipo de transição
+  balao.style.opacity = "1"; // Mostrar o balão
     }
 
-    // Função para criar o modal para usuário não logado
-    function createNotLoggedInModal() {
-        var modal = document.createElement("div");
-        modal.className = "modal";
-
-        var modalContent = document.createElement("div");
-        modalContent.className = "modal-content";
-
-        var closeButton = document.createElement("span");
-        closeButton.textContent = "×";
-        closeButton.className = "close-icon";
-
-        var message = document.createElement("p");
-        message.textContent = "Para usar seu cashback, faça login ou crie uma conta.";
-
-        modalContent.appendChild(closeButton);
-        modalContent.appendChild(message);
-        modal.appendChild(modalContent);
-
-        document.body.appendChild(modal);
-
-        closeButton.addEventListener("click", function () {
-            document.body.removeChild(modal);
-        });
+function esconderBalao() {
+  var balao = document.querySelector(".balao");
+  // Transição de opacidade para esconder o balão
+  balao.style.transition = "opacity 0.5s ease"; // Tempo e tipo de transição
+  balao.style.opacity = "0"; // Ocultar o balão
     }
 
-    // Função para criar o modal para usuário logado
-    function createLoggedInModal() {
-        var modal = document.createElement("div");
-        modal.className = "modal";
+function criarBotao() {
+  // Criar elemento <a> com a classe 'ball'
+  var link = document.createElement("a");
+  link.className = "ball";
+  link.textContent = "aqui tem cashback!";
 
-        var modalContent = document.createElement("div");
-        modalContent.className = "modal-content";
+  // Adicionar estilos CSS inline
+  link.style.background =
+    "linear-gradient(-50deg, #ff281e, #ff005a, #C00044, #D3004B, #FF143C)";
+  link.style.backgroundSize = "320% 200%";
+  link.style.animation = "Gradient 15s ease infinite";
+  link.style.color = "#fff";
+  link.style.position = "fixed";
+  link.style.zIndex = "98";
+  link.style.bottom = "25px";
+  link.style.right = "30px";
+  link.style.fontSize = "15px";
+  link.style.padding = "10px 20px";
+  link.style.borderRadius = "30px";
+  link.style.boxShadow = "0 1px 15px rgba(32, 33, 36, 0.28)";
 
-        var closeButton = document.createElement("span");
-        closeButton.textContent = "×";
-        closeButton.className = "close-icon";
-
-        var message = document.createElement("p");
-        message.textContent = "Olá, você deseja utilizar seu cashback nesta compra?";
-
-        var checkboxLabel = document.createElement("label");
-        var checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkboxLabel.textContent = " Utilizar cashback";
-        checkboxLabel.prepend(checkbox);
-
-        modalContent.appendChild(closeButton);
-        modalContent.appendChild(message);
-        modalContent.appendChild(checkboxLabel);
-        modal.appendChild(modalContent);
-
-        document.body.appendChild(modal);
-
-        closeButton.addEventListener("click", function () {
-            document.body.removeChild(modal);
-        });
+  // Adicionar animações de gradiente
+  var style = document.createElement("style");
+  style.textContent = `
+      @keyframes Gradient {
+        0% {
+          background-position: 0 50%;
     }
-
-    // Função para exibir o modal correto com base no estado de login
-    function showModalBasedOnLogin() {
-        if (isUserLoggedIn()) {
-            createLoggedInModal();
-        } else {
-            createNotLoggedInModal();
+        50% {
+          background-position: 100% 50%;
         }
+        100% {
+          background-position: 0 50%;
     }
-
-    // Exibe o modal automaticamente quando a página carrega
-    showModalBasedOnLogin();
-
-    // Estilos para o modal
-    var style = document.createElement("style");
-    style.textContent = `
-        .modal {
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            background-color: white;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
-            padding: 20px;
-            max-width: 350px;
-            max-height: 300px;
-            z-index: 9999;
-        }
-
-        .modal-content {
-            text-align: center;
-        }
-
-        .close-icon {
-            position: fixed;
-            top: 10px;
-            right: 10px;
-            font-size: 20px;
-            cursor: pointer;
-        }
-
-        .button {
-            width: 70%;
-            margin-top: 5px;
-            color: white;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        .overlay {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0, 0, 0, 0.5);
-            z-index: 9998;
-        }
-
-        .loading-animation {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: rgba(0, 0, 0, 0.5);
-            z-index: 9999;
         }
     `;
-    document.head.appendChild(style);
-}
+  document.head.appendChild(style);
+  // Adicionar o botão à página
+  document.body.appendChild(link);
+  // Adicionar evento de mouseover para mostrar o balão
+  link.addEventListener("mouseover", mostrarBalao);
+        }
+// Chamar a função para criar o botão e o balão
+criarBotao();
+criarBalao();
